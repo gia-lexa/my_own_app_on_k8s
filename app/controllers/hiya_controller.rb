0902a58552bed77a1bd9f170de013d6dcc78183e
Gia Antoniades <gia.antoniades@adhocteam.us>
2024-09-14 09:48:20 -0700

    Updates hiyas controller and route to make it make sense

--- app/controllers/hiya_controller.rb
+++ app/controllers/hiya_controller.rb
@@ -1,2 +1,7 @@
 class HiyaController < ApplicationController
+  # Simulating a collection of names in-meory instead of using a database
+  def hello
+    @name = params[:name] || 'Gia'
+    render json: { "Hello, #{name}! Welcome to Thunderdome." }
+  end
 end
