693da5b803780a767735ad5808f4cf3e957d716d
gia-lexa <103389320+gia-lexa@users.noreply.github.com>
2024-10-05 14:24:48 -0700

    Adds docker-compose.yml, updates Dockerfile to resolve gem discrepancies

--- app/controllers/hiyas_controller.rb
+++ app/controllers/hiyas_controller.rb
@@ -2,6 +2,6 @@ class HiyasController < ApplicationController
   # Simulating a collection of names in-meory instead of using a database
   def hello
     name = params[:name] || 'Gia'
-    render json: { message: "Hello, #{name}! Welcome to the party." }
+    render json: { message: "Greetings Professor #{name}. Shall we play a game?" }
   end
 end
