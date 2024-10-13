a22a48fc50b25d656de6736a3fb9b4b2746b4187
gia-lexa <103389320+gia-lexa@users.noreply.github.com>
2024-10-13 14:25:44 -0700

    rootls calculator service into port 4567

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -2,6 +2,10 @@ require 'net/http'
 require 'json'
 
 class CalculatorController < ApplicationController
+
+  set :bind, '0.0.0.0'  # Ensure it binds to all interfaces
+  set :port, 4567        # Ensure it's binding to port 4567
+
   def add
     num1 = params[:num1]
     num2 = params[:num2]
